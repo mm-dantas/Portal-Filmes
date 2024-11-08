8cb37c63fdf46827c595b843392c9cb6685bc557
mm-dantas <matheusgithub@outlook.com>
2024-11-08 17:31:17 -0300

    adicionando 'title' nas img do moviedetail

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -19,26 +19,26 @@ export default function MovieDetailPage() {
 
     const fetchMovies = async () => {
         try {
-        const elencoURL = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=7c572a9f5b3ba776080330d23bb76e1e`;
+            const elencoURL = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=7c572a9f5b3ba776080330d23bb76e1e`;
 
-        const [elencoResponse] = await Promise.all([
-            fetch(elencoURL)
-        ]);
-    
-        const elencoData = await elencoResponse.json();
+            const [elencoResponse] = await Promise.all([
+                fetch(elencoURL)
+            ]);
 
-        setElenco(elencoData.cast);
+            const elencoData = await elencoResponse.json();
+
+            setElenco(elencoData.cast);
+        }
+        catch (error) {
+            console.error('Erro ao buscar os elenco:', error);
+        }
+
     }
-    catch (error) {
-        console.error('Erro ao buscar os elenco:', error);
-    }
 
-}
+    useEffect(() => {
+        fetchMovies();
+    }, []);
 
-useEffect(() => {
-    fetchMovies();
-}, []);
-    
     //fim elesnco
 
 
@@ -115,48 +115,64 @@ useEffect(() => {
                                 <p className="text-xl font-semibold">({filme.release_date})</p>
                             </div>
                             <div className="flex items-center">
-                            <div className="w-10 h-10 flex items-center justify-center rounded-full bg-black text-white shadow-lg">
-                                <p className="flex items-center font-semibold">
-                                    {filme.vote_average}
-                                </p>
-                            </div>
-                            <p className="text-yellow-400">User Score⭐ </p>
+                                <div className="w-10 h-10 flex items-center justify-center rounded-full bg-black text-white shadow-lg">
+                                    <p className="flex items-center font-semibold">
+                                        {filme.vote_average}
+                                    </p>
+                                </div>
+                                <p className="text-yellow-400">User Score⭐ </p>
                             </div>
                         </div>
                         <p className="text-xl font-semibold max-w-xl">{filme.overview}</p>
                         <div className="flex gap-5">
-                        <button 
-                            onClick={() => handleWatchLater({ id: filme.id, title: filme.title, poster_path: filme.poster_path })} 
-                            className={`mt-4 w-16 h-16 rounded-full bg-white text-white flex items-center justify-center`}
-                        >
-                            {watchLater ? (
-                                <img src={ImgeRemoverSalvar} alt="Ícone de remover dos salvos" className="h-10"/>
+                            <button
+                                onClick={() => handleWatchLater({ id: filme.id, title: filme.title, poster_path: filme.poster_path })}
+                                className={`mt-4 w-16 h-16 rounded-full bg-white text-white flex items-center justify-center`}
+                            >
+                                {watchLater ? (
+                                    <img
+                                        src={ImgeRemoverSalvar}
+                                        alt="Ícone de remover dos salvos"
+                                        className="h-10"
+                                        title="Remover do assistir depois"
+                                    />
+                                ) : (
+                                    <img
+                                        src={ImgSalvar}
+                                        alt="Ícone de salvar"
+                                        className="h-10"
+                                        title="Adicionar ao assistir depois"
+                                    />
+                                )}
+                            </button>
 
-                            ) : (
-                                <img src={ImgSalvar} alt="Ícone de salvar" className="h-10"/>
-                                
-                            )}
-                        </button>
-                        
-                        <button 
-                            onClick={() => handleAssistido({ id: filme.id, title: filme.title, poster_path: filme.poster_path })} 
-                            className={`mt-4 w-16 h-16 rounded-full bg-white text-white flex items-center justify-center`}
-                        >
-                            {assistido ? (
-                                <img src={ImgAssistido} alt="Ícone de remover dos assistido" className="h-10"/>
+                            <button
+                                onClick={() => handleAssistido({ id: filme.id, title: filme.title, poster_path: filme.poster_path })}
+                                className={`mt-4 w-16 h-16 rounded-full bg-white text-white flex items-center justify-center`}
+                            >
+                                {assistido ? (
+                                    <img src={ImgAssistido} 
+                                    alt="Ícone de remover dos assistido" 
+                                    className="h-10"
+                                    title="Remover dos assistidos"
+                                    />
 
-                            ) : (
-                                <img src={ImgNaoAssistido} alt="Ícone adicionar aos assistidos" className="h-10"/>
-                                
-                            )}
-                        </button>
+                                ) : (
+                                    <img src={ImgNaoAssistido} 
+                                    alt="Ícone adicionar aos assistidos" 
+                                    className="h-10" 
+                                    title="Adicionar aos assistidos"
+                                    />
+
+                                )}
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div className="flex flex-col mt-12 justify-center items-center">
-            <h1 className="text-3xl font-bold mb-5">TRAILER</h1>
+                <h1 className="text-3xl font-bold mb-5">TRAILER</h1>
                 {trailer ? (
                     <iframe
                         width="1120"
@@ -176,13 +192,13 @@ useEffect(() => {
                     elenco
                         .map(pessoa => (
                             <ElencoCard
-                                key={pessoa.id} {...pessoa}/>
+                                key={pessoa.id} {...pessoa} />
                         )
                         )
                 }
             </ContainerCards>
 
-            
+
         </>
     );
 }
